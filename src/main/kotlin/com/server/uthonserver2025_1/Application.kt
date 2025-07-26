73ea67a91eed216f5f062a71c4474b1e784743f8
Simseoh <seohoon0707@gmail.com>
2025-07-26 20:24:42 +0900

    chore: deploy

--- src/main/kotlin/com/server/uthonserver2025_1/Application.kt
+++ src/main/kotlin/com/server/uthonserver2025_1/Application.kt
@@ -3,11 +3,13 @@ package com.server.uthonserver2025_1
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan
 import org.springframework.boot.runApplication
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories
 import org.springframework.transaction.annotation.EnableTransactionManagement
 
 @SpringBootApplication
 @ConfigurationPropertiesScan
 @EnableTransactionManagement
+@EnableJpaRepositories
 class Application
 
 fun main(args: Array<String>) {
